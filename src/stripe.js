1f1f5027cf43d7008aee1d0ac337c464aba8856a
PinkSquacht <77953435+PinkSquacht@users.noreply.github.com>
2024-03-22 16:35:24 -0700

    im working on adding stripe to the react app

---
+++ src/stripe.js
@@ -0,0 +1,5 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51OVFPtJbicugHtqpss9X8KAPtFqdlCc6IRsIVdRM1ItouuJ9R9un0lhz8TbFEMU7mJpYHMlOTnj5FYcpx4Xurkq000EaDKhma1');
+
+export default stripePromise;
